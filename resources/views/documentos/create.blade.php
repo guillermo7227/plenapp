8a19201639355058ee9722eae401623ab652736e
Guillermo Agudelo <guille.agudelo@gmail.com>
2020-05-15 14:42:00 -0500

    Fix boton guardar documento

--- resources/views/documentos/create.blade.php
+++ resources/views/documentos/create.blade.php
@@ -29,7 +29,7 @@
                                    accept="application/pdf"
                                    :caption="$errors->first('documento')" />
 
-                <x-ltui.button type="submit">Guardar</x-laratailui.button>
+                <x-ltui.button type="submit">Guardar</x-ltui.button>
             </form>
         </main>
     </div>
